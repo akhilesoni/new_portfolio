840d48a66b2f0019d69bd5d93b5173ec8517633a
akhilesoni <soniakhilesh101@gmail.com>
2020-09-27 20:32:40 +0530

    added git blog

--- app.js
+++
@@ -1,7 +0,0 @@
-function add(){
-var node= document.createElement("div");
-var text= document.createTextNode("hekdsd");
-node.appendChild(text);
-var element=document.getElementsByClassName('box');
-element.append(node);
-}
---
+++ like.js
@@ -0,0 +1,3 @@
+document.getElementById('like').addEventListener('click',function(){
+    document.getElementById('p1').innerHTML = "hello";
+});
